904b56ef66eca5f4783872a64313060449b681e8
FedeSca001 <familadore@gmail.com>
2024-05-18 17:03:04 +0200

    avances

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import './App.css'
+import './App.css';
 import AddCard from './Componentes/AddCard';
 
 function App() {
-  const status =['INTERESTED', 'APPLIED', 'RECRUITER INTERVIEW', 'TECH INTERVIEW', 'CLIENT INTERVIEW', 'HR INTERVIEW', 'OFFER', 'NOT SELECTED']
+  const status = ['INTERESTED', 'APPLIED', 'RECRUITER INTERVIEW', 'TECH INTERVIEW', 'CLIENT INTERVIEW', 'HR INTERVIEW', 'OFFER', 'NOT SELECTED'];
+
   return (
-    <>
-      <h1>Tech jobs tracker</h1>
+    <div className="app-container">
+      <h1>Tech Jobs Tracker</h1>
       <h3>Add and control the vacancies you are interested in</h3>
-      {status.map((item, index) => {
-        return (
-          <div key={index} className='card-container'>
-            <div className='left-content'>
+      <section className="sections-container">
+        {status.map((item, index) => (
+          <div key={index} className="card-container">
+            <div className="left-content">
               <h4>{item}</h4>
-              <AddCard nameCard={item}/>
             </div>
-            <div className='right-content'>ACA VA EL CONTENIDO LUEGO</div>
+            <div className="right-content">Content goes here</div>
           </div>
-        );
-      })}
-    </>
-  )
+        ))}
+      </section>
+      <AddCard />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Componentes/AddCard.jsx
+++ src/Componentes/AddCard.jsx
@@ -1,78 +1,94 @@
 import React, { useState } from 'react';
-import './AddCard.css'
+import './AddCard.css';
 
-function AddVacancy(props) {
-    const [companyName, setCompanyName] = useState('');
-    const [roleVacancy, setRoleVacancy] = useState('');
-    const [expectedSalary, setExpectedSalary] = useState('');
-    const [description, setDescription] = useState('');
-    const [comments, setComments] = useState('');
-    const [date, setDate] = useState(new Date().toISOString().split('T')[0]); // Estado para la fecha actual
-    const [workType, setWorkType] = useState(''); // Estado para el tipo de trabajo
+function AddVacancy() {
+  const [companyName, setCompanyName] = useState('');
+  const [roleVacancy, setRoleVacancy] = useState('');
+  const [expectedSalary, setExpectedSalary] = useState('');
+  const [description, setDescription] = useState('');
+  const [comments, setComments] = useState('');
+  const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
+  const [workType, setWorkType] = useState('');
+  const [statusType, setStatusType] = useState('');
 
-    const handleDescriptionChange = (event) => {
-        const { value } = event.target;
-        if (value.length <= 200) {
-            setDescription(value);
-        }
-    };
+  const handleDescriptionChange = (event) => {
+    const { value } = event.target;
+    if (value.length <= 200) {
+      setDescription(value);
+    }
+  };
 
-    const handleCommentsChange = (event) => {
-        const { value } = event.target;
-        if (value.length <= 80) {
-            setComments(value);
-        }
-    };
+  const handleCommentsChange = (event) => {
+    const { value } = event.target;
+    if (value.length <= 80) {
+      setComments(value);
+    }
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("Valores introducidos:", companyName, roleVacancy, expectedSalary, date, workType);
-        // Aquí puedes hacer lo que quieras con los datos introducidos
-        // Por ejemplo, enviarlos al servidor
-        setCompanyName('');
-        setRoleVacancy('');
-        setExpectedSalary('');
-        setDate(new Date().toISOString().split('T')[0]); // Resetear la fecha a la fecha actual
-        setWorkType('');
-        setDescription('');
-        setComments('');
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted values:", companyName, roleVacancy, expectedSalary, date, workType, statusType);
+    // Handle form submission here
+    setCompanyName('');
+    setRoleVacancy('');
+    setExpectedSalary('');
+    setDate(new Date().toISOString().split('T')[0]);
+    setWorkType('');
+    setDescription('');
+    setComments('');
+    setStatusType('');
+  };
 
-    return (
-        <div className="form-container">
-            <div className="input-container">
-                <input type="text" name="companyName" id="companyName" placeholder='Company Name' value={companyName} onChange={(event) => setCompanyName(event.target.value)} />
-            </div>
-            <div className="input-container">
-                <input type="text" name='roleVacancy' id='roleVacancy' placeholder='Role' value={roleVacancy} onChange={(e)=> setRoleVacancy(e.target.value)}/>
-            </div>
-            <div className="input-container">
-                <input type="date" name='date' value={date} onChange={e => setDate(e.target.value)}/>
-            </div>
-            <div className="input-container">
-                <input type="text" name='expectedSalary' placeholder='Expected Salary' value={expectedSalary} onChange={ e => setExpectedSalary(e.target.value)}/>
-            </div>
-            <div className="input-container">
-                <select value={workType} onChange={e => setWorkType(e.target.value)}>
-                    <option value="">Seleccione un tipo de trabajo</option>
-                    <option value="remote">Trabajo Remoto</option>
-                    <option value="hybrid">Trabajo Híbrido</option>
-                    <option value="inPerson">Trabajo Presencial</option>
-                </select>
-            </div>
-            <div className="input-container">
-                <textarea name="job-description" id="job-description" placeholder='Job Description' value={description} onChange={handleDescriptionChange}></textarea>
-                <span className="character-count">{description.length}/200</span>
-            </div>
-            <div className="input-container">
-                <textarea name="comments" id="comments" placeholder='Comments' value={comments} onChange={handleCommentsChange}></textarea>
-                <span className="character-count">{comments.length}/80</span>
-            </div>
-            <div className="button-container">
-                <button onClick={handleSubmit}>Submit</button>
-            </div>
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <input type="text" placeholder="Company Name" value={companyName} onChange={(event) => setCompanyName(event.target.value)} />
         </div>
-    )
+        <div className="input-container">
+          <input type="text" placeholder="Role" value={roleVacancy} onChange={(e) => setRoleVacancy(e.target.value)} />
+        </div>
+        <div className="input-container">
+          <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+        </div>
+        <div className="input-container">
+          <input type="text" placeholder="Expected Salary" value={expectedSalary} onChange={(e) => setExpectedSalary(e.target.value)} />
+        </div>
+        <div className="input-container">
+          <select value={workType} onChange={(e) => setWorkType(e.target.value)}>
+            <option value="">Select Work Type</option>
+            <option value="remote">Remote</option>
+            <option value="hybrid">Hybrid</option>
+            <option value="inPerson">In Person</option>
+          </select>
+        </div>
+        <div className="input-container">
+          <textarea placeholder="Job Description" value={description} onChange={handleDescriptionChange}></textarea>
+          <span className="character-count">{description.length}/200</span>
+        </div>
+        <div className="input-container">
+          <textarea placeholder="Comments" value={comments} onChange={handleCommentsChange}></textarea>
+          <span className="character-count">{comments.length}/80</span>
+        </div>
+        <div className="input-container">
+          <select value={statusType} onChange={(e) => setStatusType(e.target.value)}>
+            <option value="">Select Status</option>
+            <option value="INTERESTED">INTERESTED</option>
+            <option value="APPLIED">APPLIED</option>
+            <option value="RECRUITER INTERVIEW">RECRUITER INTERVIEW</option>
+            <option value="TECH INTERVIEW">TECH INTERVIEW</option>
+            <option value="CLIENT INTERVIEW">CLIENT INTERVIEW</option>
+            <option value="HR INTERVIEW">HR INTERVIEW</option>
+            <option value="OFFER">OFFER</option>
+            <option value="NOT SELECTED">NOT SELECTED</option>
+          </select>
+        </div>
+        <div className="button-container">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default AddVacancy;
